9ed0e3053bac08047435b48e207e23798917af04
Violeta Chatalbasheva <V.P.Chatalbasheva-1@student.tudelft.nl>
2024-03-20 16:01:33 +0100

    Implement MMR diversity method.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from surprise import SVD
 from surprise import dump
-from utils import split, train_and_save_model, compute_rmse, get_top_n
+from utils import split, train_and_save_model, compute_rmse, get_top_n, diversity_using_mmr  # , intra_list_diversity
 
 # Dataset taken from: "http://millionsongdataset.com/tasteprofile/"
 # user_id and song_id fields are anonymized.
@@ -12,10 +12,10 @@ dump_file = 'models/svd.pkl'
 # Load the data into a pandas dataframe
 df = pd.read_csv(data_file, sep='\t', header=None, names=['user_id', 'song_id', 'play_count'])
 
-# # Print some information about the data
-# print("Number of unique users:", df['user_id'].nunique())
-# print("Number of unique songs:", df['song_id'].nunique())
-# print("Number of records", df.shape[0])
+num_users = df['user_id'].nunique()
+num_songs = df['song_id'].nunique()
+# print(num_users)
+# print(num_songs)
 
 # # Train the model and save it to a file
 # algorithm = SVD()
@@ -37,9 +37,17 @@ song_raw_ids = [algorithm.trainset.to_raw_iid(iid) for iid in song_inner_ids]
 # TODO: how to make sure recommended songs are not already in the user's playlist?
 # One way to solve this is to filter out songs that the user has already listened to.
 
-rec_df = get_top_n(user_raw_ids[:3], song_raw_ids, algorithm, n=5)
+rec_user_ids = user_raw_ids[:1]
+rec_df = get_top_n(rec_user_ids, song_raw_ids, algorithm, n=100)
 song_info_df = pd.read_csv('data/song_data.csv')
 # Merge the recommendations with the song information
 merged_df = pd.merge(rec_df, song_info_df, on='song_id')
+
+# Add text info to compute similarity between songs (working with the current data)
+text_info = merged_df['title'] + ' ' + merged_df['release'] + ' ' + merged_df['artist_name']
+merged_df["text"] = text_info
+
+diversified_recommendation = diversity_using_mmr(merged_df, top_n=5)
+
 # Save the recommendations to a file
-merged_df.to_csv('results/recommendations.csv', index=False)
+diversified_recommendation.to_csv('results/recommendations_1user_top100_diversify.csv', index=False)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from surprise import Reader, Dataset
 from surprise.model_selection import train_test_split
 from surprise import accuracy
@@ -5,58 +7,118 @@ from surprise import dump
 import pandas as pd
 from tqdm import tqdm
 
+
 def split(df, test_size: float = 0.25):
-	"""
-	Splits a Pandas DataFrame into a Surprise trainset and a testset. 
-	Normalizes the ratings to be between 0 and 1.
-	"""
+    """
+    Splits a Pandas DataFrame into a Surprise trainset and a testset.
+    Normalizes the ratings to be between 0 and 1.
+    """
 
-	# Rename columns to match surprise's expected input
-	df.rename(columns={'user_id': 'userID', 'song_id': 'itemID', 'play_count': 'rating'}, inplace=True)
+    # Rename columns to match surprise's expected input
+    df.rename(columns={'user_id': 'userID', 'song_id': 'itemID', 'play_count': 'rating'}, inplace=True)
 
-	# Normalize ratings to be between 0 and 1
-	max_rating = df['rating'].max()
-	df['rating'] = df['rating'] / max_rating
+    # Normalize ratings to be between 0 and 1
+    max_rating = df['rating'].max()
+    df['rating'] = df['rating'] / max_rating
 
-	# Load the data into a surprise dataset
-	reader = Reader(rating_scale=(0, 1))
-	data = Dataset.load_from_df(df[["userID", "itemID", "rating"]], reader)
+    # Load the data into a surprise dataset
+    reader = Reader(rating_scale=(0, 1))
+    data = Dataset.load_from_df(df[["userID", "itemID", "rating"]], reader)
 
-	# Split the data into a trainset and a testset
-	trainset, testset = train_test_split(data, test_size=test_size)
-	return trainset, testset
+    # Split the data into a trainset and a testset
+    trainset, testset = train_test_split(data, test_size=test_size)
+    return trainset, testset
 
 
 def train_and_save_model(trainset, algorithm, dump_file):
-	"""
-	Trains a model and saves it to a file as a pickle object.
-	"""
-	algorithm.fit(trainset)
-	dump.dump(dump_file, algo=algorithm, verbose=1)
+    """
+    Trains a model and saves it to a file as a pickle object.
+    """
+    algorithm.fit(trainset)
+    dump.dump(dump_file, algo=algorithm, verbose=1)
 
 
 def compute_rmse(algorithm, testset):
-	"""
-	Computes rmse on the testset.
-	"""
-	predictions = algorithm.test(testset)
-	rmse = accuracy.rmse(predictions, verbose=True)
-	return  rmse
+    """
+    Computes rmse on the testset.
+    """
+    predictions = algorithm.test(testset)
+    rmse = accuracy.rmse(predictions, verbose=True)
+    return rmse
 
 
 def get_top_n(user_ids, song_ids, algo, n):
-	"""
-	user_id - list of raw ids of the users (uid)
-	song_ids - list of raw ids of the songs (iid)
-	n - number of recommendations to return
-	"""
-	rec = {"uid": [], "song_id": []}	
-	for uid in tqdm(user_ids):
-		user_rec = [algo.predict(uid, iid) for iid in song_ids]
-		user_rec.sort(key=lambda x: x.est, reverse=True)
-		top_n = user_rec[:n]
-		rec["uid"].extend([r.uid for r in top_n])
-		rec["song_id"].extend([r.iid for r in top_n])
+    """
+    user_id - list of raw ids of the users (uid)
+    song_ids - list of raw ids of the songs (iid)
+    n - number of recommendations to return
+    """
+    # rec = {"uid": [], "song_id": [], "rank": []}
+    recommendations = []
+    for uid in tqdm(user_ids):
+        user_rec = [algo.predict(uid, iid) for iid in song_ids]
+        user_rec.sort(key=lambda x: x.est, reverse=True)
+        top_n = user_rec[:n]
 
-	rec_df = pd.DataFrame(rec)
-	return rec_df
+        for rank, rec in enumerate(top_n, start=1):
+            recommendations.append({
+                'user_id': rec.uid,
+                'song_id': rec.iid,
+                'rank': rec.est
+            })
+
+    # print(top_n)
+    # break
+    # rec["uid"].extend([r.uid for r in top_n])
+    # rec["song_id"].extend([r.iid for r in top_n])
+    # rec["rank"].extend([r.est for r in top_n])
+
+    rec_df = pd.DataFrame(recommendations)
+    return rec_df
+
+
+def calculate_similarity(text1, text2):
+    vectorizer = TfidfVectorizer()
+    # tfidf_matrix = vectorizer.fit_transform(text_data)
+    # cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+    texts = [text1, text2]
+
+    tfidf_matrix = vectorizer.fit_transform(texts)
+    # print(tfidf_matrix)
+
+    cos_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+    # print(cos_sim)
+
+    return cos_sim[0][0]
+
+
+def diversity_using_mmr(recommendations, top_n, lambda_param=0.7):
+    recommendations['song_id'] = recommendations['song_id'].astype(str)
+    recommendations_list = recommendations.to_dict('records')
+
+    selected = []
+    remaining = recommendations_list.copy()
+
+    while len(selected) < top_n and remaining:
+        mmr_scores = []
+
+        for song in remaining:
+            song_text = song['text']
+
+            if not selected:
+                mmr_score = lambda_param * song['rank']
+            else:
+                # Compute maximum similarity to already selected songs
+                max_similarity = max(calculate_similarity(song_text, s['text']) for s in selected)
+                mmr_score = lambda_param * song['rank'] - (1 - lambda_param) * max_similarity
+                print(str(max_similarity) + " " + str(mmr_score))
+            mmr_scores.append(mmr_score)
+
+        # Select song with highest MMR score
+        max_mmr_idx = mmr_scores.index(max(mmr_scores))
+        selected_song = remaining.pop(max_mmr_idx)
+
+        selected.append(selected_song)
+
+    return pd.DataFrame(selected)
