39141f99bba319011f8af10a02f28ef58d6a6fd7
Violeta Chatalbasheva <V.P.Chatalbasheva-1@student.tudelft.nl>
2024-04-02 11:10:27 +0200

    Normalize the data for all users.

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import pandas as pd
 
 from diversify import diversity_using_mmr
+from preprocess import normalize_user
 
 pd.options.mode.chained_assignment = None
 
-df_recs = pd.read_csv("results/recommendations.tsv", sep='\t', names=['user_id', 'movie_id', 'score'])
+# Normalize the ratings for all users
+# normalize_user()
 
-# recommendations for one user
+df_recs = pd.read_csv("results/normalized_recs.tsv", sep='\t')
+
+# Get recommendations for one user
 df_1 = df_recs[df_recs['user_id'] == 2]
 
-# normalize score between 0 and 1, should be done per user
-max_score, min_score = df_1['score'].max(), df_1['score'].min()
-df_1['score'] = (df_1['score'] - min_score) / (max_score - min_score + 1e-6)
+
+top_n = 10
 
 # save diversified recommendations
-top_n = 10
-# diversed_recs = diversity_using_mmr(df_1, top_n)
-# diversed_recs.to_csv('results/diversed_recs.tsv', sep='\t', index=False, header=['movie_id', 'score'])
+diversed_recs = diversity_using_mmr(df_1, top_n)
+diversed_recs.to_csv('results/diversed_recs_1.tsv', sep='\t', index=False, header=['movie_id', 'score'])
 
 # load diversified recommendations
-diversed_recs = pd.read_csv('results/diversed_recs.tsv', sep='\t', header=0)
+diversed_recs = pd.read_csv('results/diversed_recs_1.tsv', sep='\t', header=0)
 
 
 recs = df_1[:top_n]
 
+print("user 2\n")
+print("recs:\n", recs[['movie_id', 'score']])
+print("\ndiversified recs:\n", diversed_recs)
+
+print('\nnormal score:', recs['score'].mean())
 print('diversed score', diversed_recs['score'].astype(float).mean())
-print('normal score:', recs['score'].mean())
 
 not_common = set(diversed_recs.loc[:, 'movie_id']) - set(recs.loc[:, 'movie_id'])
 print('NOT COMMON:', not_common)
---
+++ preprocess.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+pd.options.mode.chained_assignment = None
+
+def normalize_user():
+    df_recs = pd.read_csv("results/recommendations.tsv", sep='\t', names=['user_id', 'movie_id', 'score'])
+    df_recs_norm = pd.DataFrame()
+
+    user_ids = df_recs['user_id'].unique()
+    # recommendations for one user
+    for user_id in user_ids:
+        df_1 = df_recs[df_recs['user_id'] == user_id]
+
+        # normalize score between 0 and 1, should be done per user
+        max_score, min_score = df_1['score'].max(), df_1['score'].min()
+        df_1['score'] = (df_1['score'] - min_score) / (max_score - min_score + 1e-6)
+
+        # fill in dataframe
+        df_recs_norm = pd.concat([df_recs_norm, df_1])
+
+    df_recs_norm.to_csv('results/normalized_recs.tsv', sep='\t', index=False)  # header=['user_id', 'movie_id', 'score']
