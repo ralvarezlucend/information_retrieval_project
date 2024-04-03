beab1b4bb45b07d88ae2c12502db67888068e99f
Rodrigo Alvarez Lucendo <r.alvarezlucendo@student.tudelft.nl>
2024-04-03 14:32:47 +0200

    change visualization

--- visualize.py
+++ visualize.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # read the data
 recs = pd.read_csv("results/normalized_recs.tsv", sep='\t')
-recs = recs[recs['user_id'] == 2][:10] # top 10 original recommendations for user 2
+recs = recs[recs['user_id'] == 2] # top 10 original recommendations for user 2
 div_recs = pd.read_csv("results/diversed_recs.tsv", sep='\t')
 movie = pd.read_csv("new_movie_data/final_cleaned_2.csv")
 genre = pd.read_csv("mappings/ids_to_genres.csv")
@@ -21,67 +21,72 @@ rec_merge['genres'] = rec_merge['genres'].apply(lambda x: np.array([genre.loc[ge
 
 def rerank(list1, list2):
     reranking, reranked_names = [], []
-    old, new = [], []
-    
+
     for i, el1 in enumerate(list1):
         for j, el2 in enumerate(list2):
             if el1 == el2:
                 reranked_names.append(el1)
                 reranking.append({
                     "name": el1, 
-                    "method": ['ORIGINAL', 'DIVERSED'],
-                    "rank": [i+1, j+1]
+                    "method": ['ORIGINAL\nRANK', 'DIVERSIFIED\nRANK'],
+                    "old_rank": i+1,
+                    "new_rank": j+1,
+                    "rank_change": [i+1, j+1], 
+                    "is_top_n": i+1 <= 10
                 })
-                
-    for i, el1 in enumerate(list1):
-        if not el1 in reranked_names:  # noqa: E713
-            old.append({"name": el1, "rank": i+1})
-            
-    for j, el2 in enumerate(list2):
-        if not el2 in reranked_names:  # noqa: E713
-            new.append({"name": el2, "rank": j+1})
-    
-    return reranking, old, new
+
+    return reranking
 
 def plot(list1, list2):
-    reranking, old, new = rerank(rec_list, div_list)
+    reranking = rerank(list1, list2)
 
     fig, ax = plt.subplots()
+    ax.tick_params(top=False, labeltop=True, bottom=False, labelbottom=False)
+    ax.tick_params(left=False)
 
     # plot change of rank
     for el in reranking:
-        ax.plot(el["method"], 
-            el["rank"], 
+        ax.plot(
+            el["method"], 
+            el["rank_change"], 
             "o-",                       # format of marker / format of line
-            markerfacecolor="white")
+            markerfacecolor="white"    
+        )
 
-        ax.annotate(el["name"], 
-                    xy=(1, el["rank"][-1]), 
-                    xytext=(1.1, el["rank"][-1]), 
-                    va="center")
-    
-    # plot new recommendations in diversed list
-    for el in new:
-        ax.annotate(el["name"], 
-                    xy=(1, el["rank"]), 
-                    xytext=(1.1, el["rank"]), 
-                    va="center")
+        ax.annotate(
+            el["new_rank"], 
+            xy=(1, el["rank_change"][-1]), 
+            xytext=(1.1, el["rank_change"][-1]), 
+            va="center",
+            # color="black" if el["is_top_n"] else "green",
+            weight="bold" if not el["is_top_n"] else "normal"
+        )
         
-
     plt.gca().invert_yaxis()
-    top_n = 10
-    plt.yticks(np.arange(1, top_n+1))
+    
+    y_ticks = [r['old_rank'] for r in reranking if not r["is_top_n"] or r['old_rank'] <= 10]
+    y_tick_labels = [r['name'] + "       " + str(r['old_rank']) for r in reranking if not r["is_top_n"] or r['old_rank'] <= 10] 
+    plt.yticks(y_ticks, y_tick_labels)
+
 
     for spine in ax.spines.values():
         spine.set_visible(False)
 
+    plt.title(r"Movie reranking based on genres for user 2 with $\lambda$ = 0.5", loc="center", pad=30, wrap=True)
+    # make figure wider
+    fig.set_figwidth(8)
     plt.tight_layout()
-    plt.show()
-
+    # plt.show()
+    plt.savefig("results/reranking_genres_u2.pdf")
 
 rec_list = list(rec_merge['title'])
 div_list = list(div_merge['title'])
+# print(rec_list[:10])
+# print(div_list)
+plot(rec_list, div_list)
 
-# print("Original list:", rec_list)
-# print("Diversed list:", div_list)
-# plot(rec_list, div_list)
+# rec_content = rec_merge[:3]
+# rec_content['genres'] = rec_content['genres'].apply(lambda x: ', '.join(x))
+# rec_content['rank'] = np.arange(1, 4)
+# rec_table = rec_content[['rank', 'title', 'genres']].to_latex(index=False)
+# print(rec_table)
